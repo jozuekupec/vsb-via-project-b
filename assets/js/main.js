f04696dfbfb9e155f34b69ba52a969385522e85d
Jozue Kupec <jozuekupec80@gmail.com>
2024-01-07 21:53:50 +0100

    [develop] @via #b Přenastavit fonty a vyřešit responzivitu navigace

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
 class App {
     _navigationToggle;
     _body;
+    _navigation;
 
     constructor() {
         this._navigationToggle = document.querySelector(".Header-navToggle");
         this._body = document.querySelector("body");
+        this._navigation = document.querySelector(".Navigation");
         this.addEventListeners();
     }
 
@@ -12,8 +14,20 @@ class App {
         this._body.classList.toggle("Navigation--opened");
     }
 
+    /**
+     * @param {HTMLDivElement} link
+     */
+    toggleNavigationLinks(link) {
+        const linkWithSubLinksContainer = link.closest(".Navigation-subLinks");
+        linkWithSubLinksContainer.classList.toggle("Navigation-subLinks--opened");
+    }
+
     addEventListeners() {
         this._navigationToggle.addEventListener("click", () => this.toggleNavigation());
+        this._navigation.querySelectorAll(".Navigation-linkWithSubLinksContainer").forEach(link => {
+            const svgToggle = link.querySelector("[data-navigation-link-toggle]");
+            svgToggle.addEventListener("click", e => this.toggleNavigationLinks(link));
+        });
     }
 }
 
